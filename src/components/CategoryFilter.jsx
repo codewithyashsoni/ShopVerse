bf9466a5ffeec82c02b6bdafe3b3625846e1e8a9
codewithyashsoni <yashsoni.contact@gmail.com>
2026-08-02 00:27:55 +0530

    Updated categorie using products array

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -1,17 +1,21 @@
+import products from "../data/products.js"
+
 function CategoryFilter({selectedCategory, setSelectedCategory}){
+    const categories = [
+        "All",
+        ...new Set(products.map(product => product.category))
+    ];
+
     return(
         <div className="category-filter-container">
             <select 
                 className="category-filter"
                 value={selectedCategory}
                 onChange={(e) => setSelectedCategory(e.target.value)}
-            >
-                <option value="All">All</option>
-                <option value="Electronics">Electronics</option>
-                <option value="Fashion">Fashion</option>
-                <option value="Home">Home</option>
-                <option value="Sports">Sports</option>
-
+            >   
+                {categories.map((category) => (
+                    <option key={category} value={category}>{category}</option>
+                ))}
             </select>
         </div>
     )
